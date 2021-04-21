51893170e35d6379ab5eb5b1daea4731876c2270
zzza1y <66348953+zzza1y@users.noreply.github.com>
2021-04-21 16:29:07 +0800

    Add files via upload

---
+++ houtaijs.js
@@ -0,0 +1,13 @@
+﻿window.onsubmit = function()
+			{
+			var password = +1;
+			var input1 = document.getElementById("password1");
+				if(input1.value == password)
+				{
+					alert("登录成功");
+					document.getElementById("forms").action="houtai.html";
+				}
+			else{
+				alert("密码错误，登录失败");
+				}
+			}
